cb7e138283d213608d51171cf42640f3d3cfa2fe
Core <core@coredev.uk>
2022-08-16 01:09:56 +0100

    bootstrapping fix 2

--- .config/yadm/bootstrap.d/package-manager.sh
+++ .config/yadm/bootstrap.d/package-manager.sh
@@ -1,40 +1,15 @@
 #!/bin/sh
 
-system_type=$(uname -s)
+# I use arch btw
 
-# Install Homebrew on macOS
-if [ "$system_type" = "Darwin" ]; then
+if ! command -v paru >/dev/null 2>&1; then
+	echo "Installing pacman packages"
 
-  # install homebrew if it's missing
-  if ! command -v brew >/dev/null 2>&1; then
-	  echo "Installing homebrew"
-	  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
+	sudo pacman -S --needed base-devel
+	git clone https://aur.archlinux.org/paru.git
+	cd paru
+	makepkg -si
 
-  if [ -f "$HOME/.Brewfile" ]; then
-	  echo "Updating homebrew bundle"
-	  brew bundle --file ~/.config/yadm/Brewfile
-	  # Update the shell
-	  fish_path=$(which fish)
-	  chsh -s $fish_path
-  fi
-  # Install packages in Linux
-elif [ "$system_type" = "Linux" ]; then
-	os_type=$(. /etc/os-release)
-
-	if [ "$os_type" != "Arch Linux" ]; then
-		echo "This script is only for Arch Linux"
-		exit 1
-	fi
-
-	if ! command -v paru >/dev/null 2>&1; then
-		echo "Installing pacman packages"
-
-		sudo pacman -S --needed base-devel
-		git clone https://aur.archlinux.org/paru.git
-		cd paru
-		makepkg -si
-
-		paru -S git neovim neofetch alacritty
-	fi
+	paru -S git neovim neofetch alacritty
 fi
+
