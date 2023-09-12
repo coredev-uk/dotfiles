dbd10709552e8f9361d8145f929fe10076bec080
Core <64542347+coredev-uk@users.noreply.github.com>
2023-09-12 21:54:25 +0100

    test

--- .config/yadm/bootstrap.d/vim.sh
+++ .config/yadm/bootstrap.d/vim.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-if (command -v nvim &> /dev/null); then
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-fi
+directory_exists() {
+    test -d "$1"
+}
+
+
+packer_install() {
+	local PACKER_DIRECTORY="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/pack/packer/start/packer.nvim"
+
+	if ! directory_exists "$PACKER_DIRECTORY"; then
+		echo 'Installing a package manager for Neovim...'
+		git clone "https://github.com/wbthomason/packer.nvim" "$PACKER_DIRECTORY"
+		echo 'Done.'
+	fi
+}
+
+packer_install
+nvim -u NONE \
+    +'autocmd User PackerComplete quitall' \
+    +'lua require("core.plugins")' \
+    +'lua require("packer").sync()'
