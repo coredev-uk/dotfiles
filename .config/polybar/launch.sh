daf429d248b1511a68745224efee78de72b423c9
Core <64542347+coredev-uk@users.noreply.github.com>
2023-12-18 20:38:47 +0000

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,28 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Add this script to your wm startup file.
-
-DIR="$HOME/.config/polybar"
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch the bar
-# polybar -q main -c "$DIR"/config.ini &
-
-# Multiple Displays
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		tray="none"
-		if [[ $m == $(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1) ]]; then
-			tray="right"
-		fi
-		echo $m $tray
-		MONITOR=$m TRAY_POS=$tray polybar --reload -q main -c "$DIR"/config.ini &
-	done
-else
-	polybar --reload -q main -c "$DIR"/config.ini &
+if $(pgrep -x polybar > /dev/null); then
+  killall -q polybar
 fi
+
+for m in $(polybar --list-monitors | cut -d ":" -f1); do
+    t=false
+    if [ "$m" = $(polybar --list-monitors | grep 'primary' | cut -d ":" -f1) ]; then
+      t=true
+    fi
+    MONITOR=$m TRAY=$t polybar --reload main > ~/.xsession.log 2>&1 &
+done
