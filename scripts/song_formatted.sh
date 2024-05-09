885ff7e7ca63000953636dbf965eca588b917de1
Core <core@coredev.uk>
2024-05-09 15:40:07 +0100

    Various changes

--- scripts/song_formatted.sh
+++ scripts/song_formatted.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-output=$(playerctl metadata --player=cider,spotify --format '{{artist}} [ {{title}} ]' 2>&1)
+output=$(playerctl metadata --player=cider,spotify --format '{{markup_escape(artist)}} [ {{markup_escape(title)}} ]' 2>&1)
+album=$(playerctl metadata --player=cider,spotify --format '{{markup_escape(album)}}' 2>&1)
 player=$(playerctl metadata --format '{{playerName}}' 2>&1)
 
 if [[ "$output" != "No players found" && "$output" != "No player could handle this command" && "$(playerctl status 2>&1)" == "Playing" ]]; then
 
 	if [[ $1 == "--waybar" ]]; then
-		echo '{"text": "\ud83c\udfb6 '$output'", "class": "custom-nowplaying", "alt": "'$player'"}'
+		echo '{"text": "'$output'", "class": "custom-nowplaying", "alt": "'$player'", "tooltip": "'$album'"}'
 	else
 		echo $output
 	fi
