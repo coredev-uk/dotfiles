1ef82ad924a21ba0c4684cff3eb9525152e89686
Core <core@coredev.uk>
2024-07-26 02:22:00 +0100

    update

--- scripts/song_formatted.sh
+++ scripts/song_formatted.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+barType=$1
+
 escaped_artist=$(playerctl metadata --player=cider,spotify --format '{{markup_escape(artist)}}' 2>&1)
 escaped_title=$(playerctl metadata --player=cider,spotify --format '{{markup_escape(title)}}' 2>&1)
 escaped_album=$(playerctl metadata --player=cider,spotify --format '{{markup_escape(album)}}' 2>&1)
@@ -11,9 +13,9 @@ player=$(playerctl metadata --format '{{playerName}}' 2>&1)
 
 if [[ "$artist" != "No players found" && "$artist" != "No player could handle this command" && "$(playerctl status 2>&1)" == "Playing" ]]; then
 
-	if [[ $1 == "--waybar" ]]; then
+	if [[ $barType == "waybar" ]]; then
 		echo '{"text": "'$escaped_artist' [ <span color=\"#fff\">'$escaped_title'</span> ]", "class": "custom-nowplaying", "alt": "'$player'", "tooltip": "'$escaped_album'"}'
-	elif [[ $1 == "--polybar" ]]; then
+	elif [[ $barType == "polybar" ]]; then
 		echo "$artist %{F#fff}[ $title ]%{F-}"
 	fi
 else
