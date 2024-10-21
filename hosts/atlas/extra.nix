01399174dfa78f66a38c26bbdb968afb8bb4f613
Core <core@coredev.uk>
2024-10-21 21:20:06 +0100

    nah

--- hosts/atlas/extra.nix
+++ hosts/atlas/extra.nix
@@ -3,6 +3,6 @@
   imports = [ "${self}/hosts/common/services/networkmanager.nix" ];
 
   # Fix the time when Dual-booting with Windows
-  # time.hardwareClockInLocalTime = true;
-  time.timeZone = "Europe/London";
+  time.hardwareClockInLocalTime = true;
+  # time.timeZone = "Europe/London";
 }
