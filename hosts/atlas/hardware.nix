7126929e65b2b11f4acd618f6e7be262af51f4d5
Core <core@coredev.uk>
2024-10-22 17:29:33 +0100

    hypr

--- hosts/atlas/hardware.nix
+++ hosts/atlas/hardware.nix
@@ -8,18 +8,15 @@
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-    inputs.nixos-hardware.nixosModules.common-hidpi
+    # inputs.nixos-hardware.nixosModules.common-hidpi
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
 
     # ../common/hardware/bluetooth.nix
-    # ../common/hardware/yubihsm.nix
+    ../common/hardware/yubikey.nix
   ];
 
   hardware.nvidia.open = false;
-
-  #   hardware.amdgpu.amdvlk.enable = true;
-  #   hardware.amdgpu.opencl.enable = true;
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
