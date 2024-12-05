a39da338e523ee4455dbf76c21f32d4dce858a9f
Core <core@coredev.uk>
2024-12-05 18:27:13 +0000

    feat: reimplement bootwin

--- hosts/atlas/extra.nix
+++ hosts/atlas/extra.nix
@@ -2,13 +2,28 @@
   self,
   desktop,
   lib,
+  pkgs,
   ...
 }:
+let
+  bootwin = pkgs.writeScriptBin "bootwin" ''
+    #!/bin/sh
+    sudo su - <<EOF
+    efibootmgr -n 0000
+    reboot
+    EOF
+  '';
+in
 {
   imports = [
     "${self}/hosts/common/services/networkmanager.nix"
     "${self}/hosts/common/desktop/gaming.nix"
   ] ++ lib.optional (desktop == "i3") ./display.nix;
+
+  # Add bootwin script to reboot into Windows
+  environment.systemPackages = [
+    bootwin
+  ];
 
   # Fix the time when Dual-booting with Windows
   time.hardwareClockInLocalTime = true;
