79983d0f0725eef81185721593021528eeccde0f
Core <core@coredev.uk>
2024-12-10 19:49:16 +0000

    conf(mangohud): Add config

--- home/common/desktop/gaming.nix
+++ home/common/desktop/gaming.nix
@@ -7,7 +7,17 @@
 
     package = pkgs.mangohud;
 
-    settings = { };
+    settings = {
+      table_columns = 2;
+      hud_compact = true;
+      gpu_stats = true;
+      cpu_stats = true;
+      fps = true;
+      frametime = false;
+      frame_timing = false;
+      round_corners = 3;
+      width = 100;
+    };
   };
 
   home.packages = with pkgs; [
