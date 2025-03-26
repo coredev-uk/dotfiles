132b0d463c7ac88e5b960d6c627b7f23ff5d0713
Core <core@coredev.uk>
2025-03-26 13:49:25 +0000

    conf(nvim): Move to indidual file and enable more plugins

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,18 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-
 {
-  pkgs ? (import ../nixpkgs.nix) { },
-  inputs ? { },
-}:
-{
+  pkgs ? (import ../nixpkgs.nix) {},
+  inputs ? {},
+}: {
   # example = pkgs.callPackage ./example { };
   # neovim = pkgs.callPackage ./nvim { inherit nvf; };
-  nixfmt-plus = pkgs.callPackage ./nixfmt-plus.nix { };
-  cider = pkgs.callPackage ./cider.nix { };
+  nixfmt-plus = pkgs.callPackage ./nixfmt-plus.nix {};
+  cider = pkgs.callPackage ./cider.nix {};
 
   neovim =
     (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
-      modules = [ ./nvim ];
-    }).neovim;
+      modules = [./nvim.nix];
+    })
+    .neovim;
 }
---
+++ pkgs/nvim.nix
@@ -0,0 +1,121 @@
+{...}: {
+  vim = {
+    options = {
+      tabstop = 2;
+      shiftwidth = 2;
+      softtabstop = 2;
+    };
+
+    # Styling
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
+    };
+
+    # Keymaps
+    keymaps = [
+      {
+        key = "<leader>gg";
+        mode = "n";
+        silent = true;
+        action = "function() Snacks.lazygit() end";
+        lua = true;
+        desc = "Show LazyGit window";
+      }
+      {
+        key = "<leader>fr";
+        mode = "n";
+        silent = true;
+        action = "<cmd>Telescope oldfiles<CR>";
+        desc = "Show Telescopes old files";
+      }
+      {
+        key = "<leader>m";
+        mode = "n";
+        silent = true;
+        action = "<cmd>Neotree toggle<CR>";
+        desc = "Toggle filetree";
+      }
+      {
+        key = "<leader>n";
+        mode = "n";
+        silent = true;
+        action = "<cmd>Neotree reveal<CR>";
+        desc = "Reveal filetree";
+      }
+    ];
+    binds.whichKey.enable = true;
+
+    # LSP
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+      enableFormat = true;
+
+      # LSP servers
+      astro.enable = true;
+      bash.enable = true;
+      nix.enable = true;
+      python.enable = true;
+      rust.enable = true;
+      ts.enable = true;
+      tailwind.enable = true;
+    };
+
+    # Options
+    lsp = {
+      enable = true;
+
+      formatOnSave = true;
+      lspsaga.enable = true;
+    };
+
+    autocomplete.blink-cmp.enable = true;
+    autopairs.nvim-autopairs.enable = true;
+    assistant.copilot.enable = true;
+
+    # UI
+    ui.noice.enable = true;
+    notes.todo-comments.enable = true;
+    comments.comment-nvim.enable = true;
+    visuals.nvim-web-devicons.enable = true;
+
+    # mini.nvim
+    mini = {
+      basics.enable = true;
+      statusline.enable = true;
+      icons.enable = true;
+    };
+
+    # Git
+    git.gitsigns.enable = true;
+
+    # File Search
+    telescope = {
+      enable = true;
+      mappings.resume = null;
+    };
+
+    # File Tree
+    filetree.neo-tree = {
+      enable = true;
+      setupOpts.filesystem.hijack_netrw_behavior = "open_current";
+    };
+
+    utility.snacks-nvim = {
+      enable = true;
+      setupOpts = {
+        lazygit.enable = true;
+        bigfile.enable = true;
+        notifier.enale = true;
+        git.enable = true;
+        gitbrowser.enable = true;
+        scroll.enable = true;
+        indent.enable = true;
+        indent.indent.only_scope = true;
+        statuscolumn.enable = true;
+      };
+    };
+  };
+}
