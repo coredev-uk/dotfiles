2f7360a41ed17c307be3a175a6be547312d00620
Core <core@coredev.uk>
2025-03-30 17:23:27 +0100

    fix(eww): Fix workspace fetcher for eww

--- home/common/desktop/eww/config/scripts/workspace.sh
+++ home/common/desktop/eww/config/scripts/workspace.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
+
+CURRENT_MONITOR_ID="$1"
+
 i3_workspaces() {
-  all_workspaces=$(i3-msg -t get_workspaces | jq -c '.[]')
-  selected_workspace=$(i3-msg -t get_workspaces | jq --unbuffered -rc '.[] | select(.focused == true)')
+  workspaces=$(i3-msg -t get_workspaces | jq -c '.[]')
+  selected_workspace=$(echo "$all_workspaces" | jq -r 'select(.focused == true).num')
 
   buf=""
-
-  for d in $all_workspaces; do
-    name=""
+  for k in $workspaces; do
+    workspace_num=$(echo "$k" | jq -r '.num')
     class="workspace"
+    name=""
 
-    if [[ $d == *"${selected_workspace}"* ]]; then
-      name=""
+    if [[ "$workspace_num" == "$selected_workspace" ]]; then
       class="selected"
+      name=""
     fi
 
     buf="$buf (box :class \"$class\" \"$name\")"
@@ -21,40 +24,44 @@ i3_workspaces() {
 }
 
 hypr_workspaces() {
-  all_workspaces=$(hyprctl workspaces -j | jq -c '.[].id')
-  selected_workspace=$(hyprctl activeworkspace -j | jq --unbuffered -rc '.id')
+  workspaces=$(hyprctl workspaces -j | jq '.[] | del(select(.id == -99)) | .id')
+  #selected_workspace=$(hyprctl monitors -j | jq --argjson arg "$CURRENT_MONITOR_ID" '.[] | select(.id == $arg).activeWorkspace.id')
+  selected_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
 
   buf=""
-
-  for d in $all_workspaces; do
-    name=""
+  for k in $workspaces; do
+    workspace_num=$k
     class="workspace"
+    name=""
 
-    if [[ $d == "${selected_workspace}" ]]; then
-      name=""
+    if [[ "$workspace_num" == "$selected_workspace" ]]; then
       class="selected"
+      name=""
     fi
 
     buf="$buf (box :class \"$class\" \"$name\")"
   done
 
-  stdbuf -o0 echo "(box :class \"workspaces\" :halign \"center\" :valign \"center\" :vexpand true :hexpand true :spacing 10 $buf)"
+  echo "(box :class \"workspaces\" :halign \"center\" :valign \"center\" :vexpand true :hexpand true :spacing 10 $buf)"
 }
 
 workspaces() {
-  if [[ $(pgrep i3) ]]; then
+  case "$XDG_CURRENT_DESKTOP" in
+  "i3")
     i3_workspaces
     i3-msg -t subscribe -m '{"type":"workspace"}' |
-    while read -r _; do
-      i3_workspaces
-    done
-  elif [[ $(pgrep hypr) ]]; then
+      while read -r _; do
+        i3_workspaces
+      done
+    ;;
+  "Hyprland")
     hypr_workspaces
-    socat "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while read -r event; do
-    hypr_workspaces
-    done
-  fi
+    socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
+      while read -r _; do
+        hypr_workspaces
+      done
+    ;;
+  esac
 }
 
 workspaces
-
