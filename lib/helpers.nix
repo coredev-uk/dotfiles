f4a5ef6fb886436f51dea2c89014b073ba1806be
Core <core@coredev.uk>
2025-04-26 22:26:38 +0100

    conf(sys): Pass through stable packages for later use

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -19,6 +19,7 @@
     inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.unstable.legacyPackages.${system};
       extraSpecialArgs = {
+        stable = inputs.nixpkgs.legacyPackages.${system};
         inherit
           self
           inputs
@@ -47,6 +48,7 @@
     }:
     pkgsInput.lib.nixosSystem {
       specialArgs = {
+        stable = inputs.nixpkgs.legacyPackages.${system};
         inherit
           self
           inputs
