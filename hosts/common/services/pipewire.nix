2709db01b3744358d6f6c73a644775144a646e59
Core <core@coredev.uk>
2025-05-09 16:23:12 +0100

    chore: bump lockfile

--- hosts/common/services/pipewire.nix
+++ hosts/common/services/pipewire.nix
@@ -4,15 +4,10 @@
   ...
 }:
 {
-  # services.pulseaudio.enable = lib.mkForce false;
-
   services.pipewire = {
-    enable = true; # if not already enabled
+    enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
   };
 
   environment.systemPackages =
