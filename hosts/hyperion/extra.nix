302bacfc76ca7159f8fd8f59d06890ddee1c5686
Core <core@coredev.uk>
2025-06-19 23:21:59 +0100

    fix(nixidy): pacakge name and special arg

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -1,14 +1,17 @@
 {
+  inputs,
+  pkgs,
   self,
   ...
 }:
 {
   imports = [
     "${self}/hosts/common/services/networkmanager.nix"
-    "${self}/hosts/common/services/nixidy.nix"
-    {
+
+    (import "${self}/hosts/common/services/nixidy.nix" {
       hostname = "hyperion";
-    }
+      inherit inputs pkgs self;
+    })
   ];
 
   time.timeZone = "Europe/London";
