83c546149e3cc3f6a28a3a210af78ac3dc74c1a1
Core <core@coredev.uk>
2025-06-19 22:05:49 +0100

    feat(kube): Add k3s foundation

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -5,7 +5,12 @@
 {
   imports = [
     "${self}/hosts/common/services/networkmanager.nix"
+    "${self}/hosts/common/services/k3s.nix"
   ];
+
+  # K3S Kubernetes
+  services.k3s.role = "server";
+  services.k3s.clusterInit = true;
 
   time.timeZone = "Europe/London";
 
