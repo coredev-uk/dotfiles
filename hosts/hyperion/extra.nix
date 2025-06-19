3be59dfef6e58b38342ebc78add2c2f5a8b100f2
Core <core@coredev.uk>
2025-06-19 22:42:55 +0100

    fix: oops

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -5,12 +5,8 @@
 {
   imports = [
     "${self}/hosts/common/services/networkmanager.nix"
-    "${self}/hosts/common/services/k3s.nix"
+    "${self}/hosts/common/services/nixidy.nix"
   ];
-
-  # K3S Kubernetes
-  services.k3s.role = "server";
-  services.k3s.clusterInit = true;
 
   time.timeZone = "Europe/London";
 
