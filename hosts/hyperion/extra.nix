91b47cf26c1086d24aec7f36296e49070c317911
Core <core@coredev.uk>
2025-06-19 23:45:03 +0100

    fix(net): Manually set ip, dhcp not good

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -16,9 +16,28 @@
 
   time.timeZone = "Europe/London";
 
-  # DNS
-  networking.nameservers = [
-    "10.147.1.10"
-    "10.147.1.20"
-  ];
+  networking = {
+    hostName = "hyperion";
+
+    interfaces.enp170s0 = {
+      ipv4.addresses = [
+        {
+          address = "10.147.20.20";
+          prefixLength = 24;
+        }
+      ];
+      ipv4.routes = [
+        {
+          address = "0.0.0.0/0";
+          via = "10.147.20.1";
+        }
+      ];
+    };
+
+    nameservers = [
+      "8.8.8.8" # Google DNS
+      "8.8.4.4" # Google DNS (secondary)
+    ];
+  };
+
 }
