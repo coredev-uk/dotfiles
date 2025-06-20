8ffb5b943d81cba7a86135bd8c23beaa6d36e958
Core <core@coredev.uk>
2025-06-20 16:56:24 +0100

    fix(ssh): Update authorized keys

--- hosts/poseidon/boot.nix
+++ hosts/poseidon/boot.nix
@@ -13,9 +13,7 @@
     shell = pkgs.zsh;
 
     openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPF/JcM0mZ9qCfNYrAnaA/rS+N4FuQo+rGxzqAOURIktAAAACnNzaDpHaXRIdWI= YK5C-1"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIK+uHvJa0C6nT7WIm4XYyCIrJv5NOL6c55lu9TtNzQyzAAAABHNzaDo= YK5C-2"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQpQFDxvGq+x6sHldr81kFtftS6KFEzbOtoRKKTXFR7"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHp9jtjj8GUHYoLQa+PzfOOkJ9ODPc4G3YlZfYXQFqv"
     ];
 
     packages = [ pkgs.home-manager ];
