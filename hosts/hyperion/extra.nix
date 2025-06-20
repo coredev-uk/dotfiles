9073cb4674d3f5e20811956b06d03c39af662abc
Core <core@coredev.uk>
2025-06-20 01:20:07 +0100

    fix(net): Fix addresses

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -19,17 +19,13 @@
   networking = {
     hostName = "hyperion";
 
+    defaultGateway.address = "10.147.20.1";
+
     interfaces.enp170s0 = {
       ipv4.addresses = [
         {
           address = "10.147.20.20";
           prefixLength = 24;
-        }
-      ];
-      ipv4.routes = [
-        {
-          address = "0.0.0.0/0";
-          via = "10.147.20.1";
         }
       ];
     };
