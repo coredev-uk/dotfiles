39725330bc680ae50eeff3c949e48d26eb68f620
Core <core@coredev.uk>
2025-06-20 01:00:36 +0100

    chore: Add deno override

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,15 +3,21 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  modifications = _final: _prev: {
+  modifications = _final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
 
-    # Can be removed once https://github.com/NixOS/nixpkgs/pull/338960 is in unstable.
-    # swaylock-effects = prev.swaylock-effects.overrideAttrs (_: {
-    #   depsBuildBuild = [ prev.pkg-config ];
-    # });
+    # Removed once https://github.com/NixOS/nixpkgs/pull/416177 is in nixos-unstable
+    deno = prev.deno.overrideAttrs (oldAttrs: {
+      version = "2.3.6";
+      src = prev.fetchFromGitHub {
+        owner = "denoland";
+        repo = "deno";
+        rev = "v2.3.6";
+        hash = "sha256-l3cWnv2cEmoeecYj38eMIlgqlRjDbtQuc6Q3DmOJoqE=";
+      };
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
