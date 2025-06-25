883a8af2773724f8b484bf2e39304b0e65200923
Core <core@coredev.uk>
2025-06-25 09:40:20 +0100

    fix: Actually add the k3s file

--- hosts/common/services/k3s.nix
+++ hosts/common/services/k3s.nix
@@ -1,5 +1,11 @@
-{ lib, config, ... }:
 {
+  lib,
+  config,
+  self,
+  ...
+}:
+{
+  age.secrets.k3s.file = "${self}/secrets/k3s.age";
 
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
