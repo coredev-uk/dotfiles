1fc39979fd830c411cdc8e8de2570a85bfa64a98
Core <core@coredev.uk>
2025-06-25 10:13:26 +0100

    fix: change keys again

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,17 @@
 let
-  paul = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHp9jtjj8GUHYoLQa+PzfOOkJ9ODPc4G3YlZfYXQFqv";
+  sysadmin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCmA4pOckYwJCeDUMOrRCU5xX2g1x9Ddsf3eDa4UViX";
   users = [
-    paul
+    sysadmin
   ];
 
-  hyperion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCmA4pOckYwJCeDUMOrRCU5xX2g1x9Ddsf3eDa4UViX";
+  hyperion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICL9+Cp3dUkcgvzGUaDcfsoywXeaRISJNZ/Tgqqzlk6k";
   systems = [
     hyperion
   ];
 in
 {
-  "k3s.age".publicKeys = users ++ systems;
+  "hyperion.age".publicKeys = [
+    hyperion
+    sysadmin
+  ];
 }
