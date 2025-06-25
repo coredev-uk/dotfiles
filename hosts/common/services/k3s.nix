6444c744add0530296cbac2bd4e860b03045cdb3
Core <core@coredev.uk>
2025-06-25 07:39:45 +0100

    feat: Re-add k3s

---
+++ hosts/common/services/k3s.nix
@@ -0,0 +1,23 @@
+{ lib, ... }:
+{
+
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+
+  services.k3s = {
+    enable = true;
+    role = lib.mkDefault "agent"; # or "server"
+    token = "unicorn-drive-turbulence-road";
+    extraFlags = toString [
+      "--debug"
+    ];
+    # clusterInit = true;
+    # serverAddr = "https://<ip of first node>:6443";
+  };
+}
