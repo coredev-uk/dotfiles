bb2c3d26013a39107bdf2360f8351e593bd5e319
Core <core@coredev.uk>
2025-06-25 12:20:23 +0100

    fix: add longhorn fixes and disable cli flags

--- hosts/common/services/k3s.nix
+++ hosts/common/services/k3s.nix
@@ -5,6 +5,12 @@
 }:
 {
 
+  # Fixes for longhorn
+  systemd.tmpfiles.rules = [
+    "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
+  ];
+  virtualisation.docker.logDriver = "json-file";
+
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
@@ -20,11 +26,11 @@
     tokenFile = config.age.secrets.${meta.hostname}.path;
     extraFlags = toString (
       [
-        "--write-kubeconfig-mode \"0644\""
-        "--cluster-init"
-        "--disable servicelb"
-        "--disable traefik"
-        "--disable local-storage"
+        # "--write-kubeconfig-mode \"0644\""
+        # "--cluster-init"
+        # "--disable servicelb"
+        # "--disable traefik"
+        # "--disable local-storage"
       ]
       ++ (
         if meta.hostname == "hyperion" then
