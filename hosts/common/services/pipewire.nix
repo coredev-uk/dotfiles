c91b3f27521ba1f073bf0905c04f430326b3f5eb
Core <core@coredev.uk>
2025-08-17 16:08:36 +0100

    fix(pipewire): Add custom pipewire setting to fix headset issues

--- hosts/common/services/pipewire.nix
+++ hosts/common/services/pipewire.nix
@@ -8,6 +8,20 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+
+    extraConfig.pipewire = {
+      context.properties = {
+        # https://discourse.nixos.org/t/crackling-pipewire/21774/2
+        default.clock.allowed-rates = [
+          44100
+          48000
+          96000
+        ];
+        default.clock.quantum = 256;
+        default.clock.min-quantum = 256;
+        default.clock.max-quantum = 1024;
+      };
+    };
   };
 
   environment.systemPackages =
