1028947e8e51949f262a2b4aef9485994922e95f
Core <core@coredev.uk>
2025-09-07 23:48:09 +0100

    fix: dumb

--- hosts/hyperion/extra.nix
+++ hosts/hyperion/extra.nix
@@ -20,21 +20,22 @@
 
     defaultGateway.address = "10.147.20.1";
 
-    routes = [
-      {
-        address = "10.147.5.0";
-        prefixLength = 24;
-        via = "10.147.11.1";
-      }
-    ];
-
     interfaces.enp170s0 = {
-      ipv4.addresses = [
-        {
-          address = "10.147.20.20";
-          prefixLength = 24;
-        }
-      ];
+      ipv4 = {
+        addresses = [
+          {
+            address = "10.147.20.20";
+            prefixLength = 24;
+          }
+        ];
+        routes = [
+          {
+            address = "10.147.5.0";
+            prefixLength = 24;
+            via = "10.147.11.1";
+          }
+        ];
+      };
     };
 
     nameservers = [
