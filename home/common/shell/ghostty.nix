00ffb27feaee6dbfeab8f741e5e87feaa3f73da9
Core <core@coredev.uk>
2025-09-20 16:24:43 +0100

    fix(ghostty): change to hm package and fix theme name

--- home/common/shell/ghostty.nix
+++ home/common/shell/ghostty.nix
@@ -7,14 +7,14 @@ let
   theme = import "${self}/lib/theme" { inherit pkgs; };
 in
 {
-  home.file.".config/ghostty/config".text = ''
-    theme = catppuccin-${theme.catppuccin.flavor}
-    font-family = "${theme.fonts.monospace.name}"
-    window-decoration = false
-    clipboard-paste-protection = false
-  '';
-
-  home.packages = with pkgs; [
-    ghostty
-  ];
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      theme = "Catppuccin Mocha";
+      font-family = theme.fonts.monospace.name;
+      window-decoration = false;
+      clipboard-paste-protection = false;
+    };
+  };
 }
