c85b49e42289b939b9440aad71971ffbce3cecd9
Core <core@coredev.uk>
2025-10-23 20:28:46 +0100

    fix(hyprsunset): add second profile

--- home/common/programs/hypr/sunset.nix
+++ home/common/programs/hypr/sunset.nix
@@ -7,10 +7,13 @@ _: {
           time = "7:30";
           identity = true;
         }
-
+        {
+          time = "20:00";
+          temperature = 4500;
+        }
         {
           time = "21:00";
-          temperature = 5500;
+          temperature = 4000;
         }
       ];
     };
