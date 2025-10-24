f61ef523b8d2d1650145c528ee9df7834f9bb77b
Core <core@coredev.uk>
2025-10-24 03:30:54 +0100

    fix: enable ags over eww

--- home/common/desktop/hyprland/default.nix
+++ home/common/desktop/hyprland/default.nix
@@ -52,7 +52,8 @@
         };
 
         exec-once = [
-          "eww open-many bar bar-second"
+          # "eww open-many bar bar-second"
+          "MANGOHUD=0 ags run"
           "${wallpaper}/bin/get-wallpaper --session=hyprland"
         ];
 
