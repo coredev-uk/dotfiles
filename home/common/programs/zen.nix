32a34234907a5e89dc1b73f0ff43d362f2573579
Core <core@coredev.uk>
2025-11-02 02:29:28 +0000

    feat(zen): add policies
    only standard ones

--- home/common/programs/zen.nix
+++ home/common/programs/zen.nix
@@ -2,6 +2,26 @@
 {
   programs.zen-browser = {
     enable = true;
+
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+
     profiles."default" = {
 
       # User Containers (With Facebook Containers enabled)
