80f8edee7e19e97ed797760e711e80807e225c8e
Core <core@coredev.uk>
2025-11-07 16:16:31 +0000

    fix: use discord over nixcord on mac
    No idea but always fails to open saying its damaged.

--- home/hosts/poseidon.nix
+++ home/hosts/poseidon.nix
@@ -1,10 +1,14 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
 
   imports = [
-    ../common/programs/discord.nix
+    # ../common/programs/discord.nix
     ../common/programs/ghostty.nix # NixOS/nixpkgs#388984
     ../common/programs/zen.nix
+  ];
+
+  home.packages = with pkgs; [
+    discord
   ];
 
   programs.ghostty.enable = lib.mkForce false;
